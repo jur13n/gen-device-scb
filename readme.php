cceed700a4c0ff3c659c47efa4fa206be0d8db70
jur13n <158059416+jur13n@users.noreply.github.com>
2024-01-29 21:08:20 +0700

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,14 @@
+<?php
+
+$urls = [
+    "https://fasteasy.scbeasy.com:8443/v3/login/preloadandresumecheck",
+    "https://fasteasy.scbeasy.com:8443/v1/login/getMigrationFlag",
+    "https://fasteasy.scbeasy.com:8443/v1/registration/verifyuser",
+    "https://fasteasy.scbeasy.com:8443/v1/profiles/generateOTP",
+    "https://fasteasy.scbeasy.com:8443/v2/profiles/allowadddevice",
+    "https://fasteasy.scbeasy.com:8443/isprint/soap/preAuth",
+    "https://fasteasy.scbeasy.com:8443/v1/fasteasy-login",
+    "https://fasteasy.scbeasy.com:8443/v1/profiles/devices/"
+];
+
+?>
